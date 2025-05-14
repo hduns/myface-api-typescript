862877b119b68baecc3446db52d3c2a99954c8b0
Simon Duck <its.duck@gmail.com>
2025-05-14 11:31:04 +0100

    users and router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PostsPage from './ThePostsPage/PostsPage'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import UsersPage from './TheUsersPage/UsersPage'
 
 function App() {
   const [myData, setMyData] = useState(JSON);
@@ -11,10 +12,22 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="./ThePostsPage"
+        <Route path="/ThePostsPage"
                element={<PostsPage myData={myData} setMyData={setMyData}/>}/>
-        <Route path="./ThePostsPage"
-               element={<PostsPage myData={myData} setMyData={setMyData}/>}/>
+        <Route path="/TheUsersPage"
+               element={<UsersPage myData={myData} setMyData={setMyData}/>}/>
+
+               <Route path="*"
+              element={<div>
+              Sorry - that page doesn't exist, try these:
+                    <div>
+                    <Link to="/ThePostsPage">Posts</Link>
+                    </div>
+                    <div>
+                    <Link to="/TheUsersPage">Users</Link>
+                    </div>
+             
+              </div>}/>
       </Routes>
     </Router>
   )
--- frontend/src/TheUsersPage/UsersPage.tsx
+++ frontend/src/TheUsersPage/UsersPage.tsx
@@ -3,4 +3,4 @@ import { useEffect } from "react";
 export default function UsersPage(props: {myData: JSON, setMyData: (data: JSON) => void}) {
     useEffect(() => {
         fetch("http://localhost:3001/users").then(response => response.json()).then(data => props.setMyData(data.results))
-    }, []);
+    }, []);}
