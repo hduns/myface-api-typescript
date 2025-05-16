d6620c7a0f5004b7c6da07594418ba0b3dc639f4
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-05-16 12:05:15 +0100

    added form validation

--- frontend/src/CreateUserPage/CreateUser.tsx
+++ frontend/src/CreateUserPage/CreateUser.tsx
@@ -13,10 +13,34 @@ type FormData = {
 
 export default function CreateUser() {
     const [newUserData, setNewUserData ] = useState<FormData>({} as FormData);
+    const [message, setMessage ] = useState<string>('');
     
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
     const value = event.target.value;
+    const pattern = event.target.pattern;
+    
+    if (name != "email") {
+        if (new RegExp(pattern).test(value)) {
+            setMessage("");
+        } else {
+            switch(name) {
+                case "name":
+                    setMessage("Names can only contain letters and spaces");
+                    break;
+                case "username":
+                    setMessage("Usernames must be lowercase with no spaces");
+                    break;
+            }
+        }
+    } else {
+        setMessage("Please complete a valid email address");
+        let emailRegex =/^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/
+
+        if (emailRegex.test(value)) {
+            setMessage("");
+        }
+    }
     setNewUserData(values => ({...values, [name]: value}));
   };
 
@@ -41,20 +65,23 @@ export default function CreateUser() {
     return (
 
         <form className="CreateNewUserForm"  onSubmit={handleSubmit}> 
+            <div>
+                <p>{message}</p>
+            </div>
             <label className="newUserName">
                 <p id="Name">Name:</p>
-                <input id="NameInput" type="text" name="name" pattern="[a-z0-9]+" placeholder="Enter name here" value={newUserData.name || ""} onChange={handleChange} required/>
+                <input id="NameInput" type="text" name="name" pattern="[a-zA-Z ]+$" placeholder="Enter name here" value={newUserData.name || ""} onChange={handleChange} required/>
             </label>
             
             <label className="newUserUserame">
                 <p id="Username">Username:</p>
-                <input id="UsernameInput" type="text" name="username" pattern="[a-z0-9]+" placeholder="Enter username here" value={newUserData.username || ""} onChange={handleChange} required/>
+                <input id="UsernameInput" type="text" name="username" pattern="[a-z0-9]+$" placeholder="Enter username here" value={newUserData.username || ""} onChange={handleChange} required/>
 
             </label>
 
             <label className="newUserEmail">
                 <p id="Email">Email:</p>
-                <input id="EmailInput" type="email" name="email"  placeholder="Enter email here"  value={newUserData.email || ""} onChange={handleChange}  required/>
+                <input id="EmailInput" type="text" name="email" placeholder="Enter email here"  value={newUserData.email || ""} onChange={handleChange}  required/>
             </label>
             
             <label className="newUserProfileImageUrl">
